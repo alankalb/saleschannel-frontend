abc857f65ece3864c5eee844ca2e5b37a6d090c0
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2019-06-22 10:47:43 -0400

    created html view for main route

--- app.js
+++ app.js
@@ -5,20 +5,11 @@ const router = express.Router();
 const port = parseInt(process.env.PORT, 10) || 3000
 
 router.get('/',function(req,res){
-  res.send('<p>some html</p>')
-  //__dirname : It will resolve to your project folder.
-});
-/*
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
-*/
-//add the router
 app.use('/', router);
-app.listen(port);
+app.listen(port, function(){
+  console.log('Running at Port ' + port);
+});
 
-console.log('Running at Port 3000');
